5b23a9612fc905b6b72aabc3e90a953ed873547f
Eric <eric.bujaldon.7e2@itb.cat>
2021-05-05 18:11:13 +0200

    RTSBase added

---
+++ Assets/Scripts/RTSBase.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSBase : MonoBehaviour
+{
+
+    int health;
+    string entityName;
+    int maxHealth;
+    string description;
+    Sprite preview;
+    GameObject prefab;
+
+
+    public void Start()
+    {
+        
+    }
+
+    void SetColor() { }
+    void SetSelected(bool isSelected)
+    {
+        transform.Find("Highlight").gameObject.SetActive(isSelected);
+
+    }
+    void TakeDamage(UnitController enemy, float damage)
+    {
+        StartCoroutine(Flasher(GetComponent<Renderer>().material.color));
+
+    }
+    IEnumerator Flasher(Color defaultColor)
+    {
+        var renderer = GetComponent<Renderer>();
+        for (int i = 0; i < 2; i++)
+        {
+            renderer.material.color = Color.gray;
+            yield return new WaitForSeconds(.05f);
+            renderer.material.color = defaultColor;
+            yield return new WaitForSeconds(.05f);
+        }
+    }
+}

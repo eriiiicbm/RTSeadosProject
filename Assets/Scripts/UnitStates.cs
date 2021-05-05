7d1778e9a76f60de31dadca1a39957b1c9d48b04
CortesJoan <joan.cortes.7e3@itb.cat>
2021-05-05 14:43:26 +0200

    Example od stateMachine,enums and interfaces

---
+++ Assets/Scripts/UnitStates.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum  UnitStates
+{
+Idle,Follow,Dead,Escape,Attack
+}
+public enum EmotionalStates {
+Neutral,Aggressive, Pasive
+}
+

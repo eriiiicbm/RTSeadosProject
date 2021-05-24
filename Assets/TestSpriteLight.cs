8116f06fbd461d15a27f983c59f5edb4209cc468
Cortés <joan.cortes.7e3@itb.cat>
2021-05-24 13:09:29 +0200

    Added day/night system

---
+++ Assets/TestSpriteLight.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+public class TestSpriteLight : MonoBehaviour
+{ 
+    SpriteRenderer sprite;
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    //Variables
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            //(Replace with a reference to the game time)
+            TimeOfDay += Time.deltaTime/50;
+            TimeOfDay %= 24; //Modulus to ensure always between 0-24
+            UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
+
+    private void UpdateLighting(float timePercent)
+    {
+        //Set ambient and fog
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
+
+        //If the directional light is set then rotate and set it's color, I actually rarely use the rotation because it casts tall shadows unless you clamp the value
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+    }
+
+
+
+  
+    // Start is called before the first frame update
+    void Start()
+    { 
+        sprite = GetComponentInParent<SpriteRenderer>();
+    }
+
+ 
+}

ff0a0d0d8cc6953336dcbe411c96ec4c034d32dc
Cortés <joan.cortes.7e3@itb.cat>
2021-05-29 00:57:54 +0200

    Rollback UnitBasev2

--- Assets/Scripts/MultiplayerV2/Buildings/UnitBasev2.cs
+++ Assets/Scripts/MultiplayerV2/Buildings/UnitBasev2.cs
@@ -6,51 +6,39 @@ using UnityEngine;
 
 public class UnitBasev2 : Building
 {
-    [SerializeField] public static event Action<int> ServerOnPlayerDie;
-    [SerializeField] public static event Action<UnitBasev2> ServerOnBaseSpawned;
-    [SerializeField] public static event Action<UnitBasev2> ServerOnBaseDespawned;
-
+    [SerializeField] public  static  event Action<int> ServerOnPlayerDie;
+    [SerializeField] public  static  event Action<UnitBasev2> ServerOnBaseSpawned;
+    [SerializeField] public  static  event Action<UnitBasev2> ServerOnBaseDespawned;
+ 
 
     #region Server
-
     public override void OnStartServer()
     {
         base.OnStartServer();
         ServerOnRTSDie += ServerHandleDie;
-        RTSPlayerv2.ClientOnResourcesUpdated += HandleResourcesUpdated;
         ServerOnBaseSpawned?.Invoke(this);
-
-    }
-
-    private void HandleResourcesUpdated(SyncList<int> obj)
-    {
-         RTSPlayerv2 playerv2 = connectionToClient.identity.GetComponent<RTSPlayerv2>();
-        Debug.Log($"time to die for {playerv2.name}");
-        DealDamage(maxHealth);
-        
+       
     }
 
     public override void OnStopServer()
     {
 
         base.OnStopServer();
+        ServerOnBaseDespawned?.Invoke(this);
         ServerOnRTSDie -= ServerHandleDie;
-        RTSPlayerv2.ClientOnResourcesUpdated -= HandleResourcesUpdated;
 
     }
-
     [Server]
     private void ServerHandleDie()
     {
         ServerOnPlayerDie?.Invoke(connectionToClient.connectionId);
         NetworkServer.Destroy(gameObject);    }
-
-    #endregion
  
+    #endregion
 
     #region Client
 
  
 
- #endregion
+    #endregion
 }

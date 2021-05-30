d0e4e161ba71fc510fccabd18bfee225d45f1bde
Cortés <joan.cortes.7e3@itb.cat>
2021-05-30 02:02:56 +0200

    FIXED  ALL THE  TOWER BUGS

--- Assets/Scripts/Builds/Tower.cs
+++ Assets/Scripts/Builds/Tower.cs
@@ -12,48 +12,66 @@ public class Tower : Building
     float attackSpeed;
     float throwForce = 5;
     GameObject proyectils;
+    [SerializeField] GameObject towerCannon;
     [SerializeField] private LayerMask layerMask = new LayerMask();
     private Transform lookAt;
+
     public void detectEnemy(float atackRange)
     {
-      Collider[] collider =   Physics.OverlapSphere(this.gameObject.transform.position, atackRange,layerMask);
-      if (collider.Length==0)
-      {
-          return;
-      }
-      Collider nearCollider = collider[0];
-      foreach (var col in collider)
-      {
-          if (col.GetComponent<RTSBase>().connectionToClient==null)
-          {
-              goto afterConnectionCheck;
-          }
-          if (col.GetComponent<RTSBase>().connectionToClient.connectionId ==
-              NetworkClient.connection.connectionId) continue;
-          afterConnectionCheck:
-          if (Vector3.Distance(col.transform.position,this.transform.position) <= Vector3.Distance(nearCollider.transform.position,this.transform.position)  )
-          {
-              nearCollider = col;
-          }
-      }
-      if (nearCollider == null) return;
-      
-      RTSBase enemy = nearCollider.GetComponent<RTSBase>();
-   
-      if (enemy == null) return;
-      if (enemy.connectionToClient==null)
-      {
-          goto afterConnectionCheck2;
-      }
-      if (enemy.connectionToClient.connectionId ==
-          NetworkClient.connection.connectionId)
-      {
-          return;
-      }
-      afterConnectionCheck2:
-      Debug.Log($"{enemy.name} detected");
+        Collider[] collider = Physics.OverlapSphere(this.gameObject.transform.position, atackRange, layerMask);
+        if (collider.Length == 0)
+        {
+            return;
+        }
+
+        Collider nearCollider = collider[0];
+        foreach (var col in collider)
+        {
+            if (col.GetComponent<RTSBase>().connectionToClient == null)
+            {
+                continue;
+            }
+
+            if (col.GetComponent<RTSBase>().connectionToClient.connectionId ==
+                NetworkClient.connection.connectionId) continue;
+            if (Vector3.Distance(col.transform.position, this.transform.position) <=
+                Vector3.Distance(nearCollider.transform.position, this.transform.position))
+            {
+                nearCollider = col;
+            }
+        }
+
+        if (nearCollider == null) return;
+
+        RTSBase enemy = nearCollider.GetComponent<RTSBase>();
+
+        if (enemy == null) return;
+        if (enemy.connectionToClient == null)
+        {
+            return;
+        }
+
+        if (enemy.connectionToClient.connectionId ==
+          connectionToClient.connectionId)
+        {
+            return;
+        }
+
+        transform.LookAt(enemy.transform.position);
+
+        Vector3 eulerAngles = transform.rotation.eulerAngles;
+        eulerAngles.x = 0;
+        eulerAngles.z = 0;
+        eulerAngles.y -= 180;
+
+        transform.rotation = Quaternion.Euler(eulerAngles);
+
+        if (!(attackSpeed >= attackTimer)) return;
+        Debug.Log("Time to attack");
+        Debug.Log($"{enemy.name} detected");
         GetComponent<ComponentAbility>().SetParameter(rtsEntity.EffectRadious);
         GetComponent<ComponentAbility>().active(enemy, damege);
+        attackSpeed = 0;
     }
 
     // Start is called before the first frame update
@@ -65,30 +83,27 @@ public class Tower : Building
         damege = rtsEntity.Damage;
         proyectils = rtsEntity.Proyectile;
         throwForce = rtsEntity.Velocity;
-        
-        
+
+
         attackTimer = rtsEntity.AttackTimer;
     }
 
     // Update is called once per frame
-   [ServerCallback]
+    [ServerCallback]
     void Update()
     {
-         attackSpeed += Time.deltaTime;
+        attackSpeed += Time.deltaTime;
 
-   //     Debug.Log($"{attackSpeed} attackspeed");
+        //     Debug.Log($"{attackSpeed} attackspeed");
 
         if (!base.builded) return;
         Debug.Log("Tower is builded");
-        if (  !(attackSpeed >= attackTimer)) return;
-        Debug.Log("Time to attack");
 
         detectEnemy(attackRange);
-         attackSpeed = 0;
-
     }
 
     private void OnDrawGizmosSelected()
     {
-Gizmos.DrawSphere(this.gameObject.transform.position, attackRange);    }
+        Gizmos.DrawSphere(this.gameObject.transform.position, attackRange);
+    }
 }
